a00a6eb11df5c567d30594b314c726794b2874e0
Julia Makhlin <tourdefiurst@yandex.com>
2022-05-02 22:42:39 +0300

    fix/удалить required в aboutValidation

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -41,8 +41,8 @@ const cardValidation = celebrate({
 
 const userAboutValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
   }),
 });
 
