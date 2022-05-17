3aa0bb0102ee8fae9ec9dedd9f997f687073dbff
Julia Makhlin <tourdefiurst@yandex.com>
2022-05-17 03:25:40 +0300

    исправить заголовки

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
   }
   if (method === 'OPTIONS') {
