6cbdaafdc4f306eb64ded8fdfedf1fdfc8c769ea
Julia Makhlin <tourdefiurst@yandex.com>
2022-05-18 08:38:29 +0300

    fix/исправит/

--- app.js
+++ app.js
@@ -18,7 +18,9 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 const allowedCors = [
+  'http://mesto.juliamakhlin.nomoredomains.xyz/',
   'https://mesto.juliamakhlin.nomoredomains.xyz/',
+  'http://api.mesto.juliamakhlin.nomoredomains.xyz/',
   'https://api.mesto.juliamakhlin.nomoredomains.xyz/',
   'localhost:3000',
 ];
